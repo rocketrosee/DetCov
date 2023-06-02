485bdcb40be71835d093d29c6fdf182eba1a6dc5
RAKA <133620607+rocketrosee@users.noreply.github.com>
2023-06-02 10:33:53 +0700

    Create RakaAP-245.java

---
+++ RakaAP-245.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.raka245;
+
+/**
+ *
+ * @author rakaap
+ */
+public class RAKA245 {
+    public static void main(String[] args) {
+        System.out.println("RAKA ARYADITYA PUTRATAMA");
+        System.out.println("2113010245");
+    }
+}
