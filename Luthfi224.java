2fb1d39b02df6ca6fae930eae2409da3518d8819
upy666 <135245443+upy666@users.noreply.github.com>
2023-08-18 20:55:36 +0700

    2113010224

---
+++ Luthfi224.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.luthfi224;
+
+/**
+ *
+ * @author luthfi
+ */
+public class Luthfi224 {
+    public static void main(String[] args) {
+        System.out.println("Muhammad Luthfi Hakim");
+        System.out.println("2113010224");
+    }
+}
